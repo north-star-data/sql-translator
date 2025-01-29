ce0300c772b2442bfcaf0b709e99a001b0471881
Emil Sadek <esadek@users.noreply.github.com>
2025-01-29 01:55:44 -0800

    Add: application

---
+++ app.py
@@ -0,0 +1,65 @@
+import sqlglot
+import streamlit as st
+from streamlit_ace import st_ace
+
+dialects = (
+    "athena",
+    "bigquery",
+    "clickhouse",
+    "databricks",
+    "doris",
+    "drill",
+    "druid",
+    "duckdb",
+    "hive",
+    "materialize",
+    "mysql",
+    "oracle",
+    "postgres",
+    "presto",
+    "prql",
+    "redshift",
+    "risingwave",
+    "snowflake",
+    "spark",
+    "spark2",
+    "sqlite",
+    "starrocks",
+    "tableau",
+    "teradata",
+    "trino",
+    "tsql",
+)
+
+st.set_page_config(page_title="SQL Translation", layout="wide")
+
+st.title("SQL Translator")
+st.write(f"Translate SQL queries between {len(dialects)} different dialects")
+st.markdown(
+    "[![Built by North Star](https://img.shields.io/badge/built_by-North_Star_Data-4F46E5)](https://northstardata.co/)"
+)
+st.divider()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    input_dialect = st.selectbox(label="Input", options=dialects, index=7)
+    input_query = st_ace(
+        value="SELECT EPOCH_MS(1618088028295)",
+        placeholder="Write your SQL query here",
+        language="sql",
+        theme="dracula",
+    )
+
+with col2:
+    output_dialect = st.selectbox(label="Output", options=dialects, index=8)
+    output_query = sqlglot.transpile(
+        sql=input_query, read=input_dialect, write=output_dialect, pretty=True
+    )[0]
+    output = st_ace(
+        value=output_query,
+        language="sql",
+        theme="dracula",
+        readonly=True,
+        auto_update=True,
+    )
